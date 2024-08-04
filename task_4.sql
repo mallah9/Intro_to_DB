cb5785f752d950f56f8795696c5771686c21e8bf
mallah9 <mahmoud_abdalla@live.com>
2024-08-05 00:06:01 +0300

    task4

--- task_4.sql
+++ task_4.sql
@@ -1,2 +1,14 @@
 USE alx_book_store;
-SHOW FULL COLUMNS FROM Books;
+
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE, 
+    COLUMN_KEY, 
+    EXTRA, 
+    COLUMN_COMMENT 
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
