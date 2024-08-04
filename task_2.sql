d9663a72a99068b5b115daa1f0eaf5de14ee1990
mallah9 <mahmoud_abdalla@live.com>
2024-08-04 22:33:14 +0300

    resolve task2, creating tables script

--- task_2.sql
+++ task_2.sql
@@ -44,7 +44,7 @@ CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY AUTO_INCREMENT,
     order_id INT,
     book_id INT,
-    quantity INT,
+    quantity DOUBLE,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
