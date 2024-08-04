9ae5ea1f07ff37205b84213b878261814df86fe3
mallah9 <mahmoud_abdalla@live.com>
2024-08-05 00:08:23 +0300

    task_5

--- task_4.sql
+++ task_4.sql
@@ -10,5 +10,6 @@ SELECT
 FROM 
     INFORMATION_SCHEMA.COLUMNS 
 WHERE 
-    TABLE_SCHEMA = 'alx_book_store' 
-    AND TABLE_NAME = 'Books';
+    TABLE_SCHEMA = "alx_book_store" 
+    AND TABLE_NAME = "Books";
+    
